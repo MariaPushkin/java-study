40d91867f85100003d79402924e7abd25e1c16f7
Mary <rtutmash@gmail.com>
2017-11-09 20:07:37 +0300

    Add calculations

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,18 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
-		System.out.println("Done");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double summ = first + second;
+		double diff = first - second;
+		double comp = first * second;
+		double div;
+		if (second != 0) 
+			div = first / second;
+		else div = 0;
+		System.out.println("Sum - " + summ);
+		System.out.println("Dif - " + diff);
+		System.out.println("Comp - " + comp);
+		System.out.println("Div - " + div);
 	}
 }
