1914b711114f9476932c87fc6535725b2f8f5cd5
Mary <rtutmash@gmail.com>
2017-11-12 20:30:15 +0300

    Add other actions

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,28 @@ public class Calculator {
 		}
 	}
 	
+	//вычитание
+	public void remove(int ... params) {
+		for(Integer param : params) {
+			this.result -= param;
+		}
+	}
+	
+	//умножение
+	public void mult(int ... params) {
+		for(Integer param : params) {
+			this.result *= param;
+		}
+	}
+		
+	//деление
+	public void div(int ... params) {
+		for(Integer param : params) {
+			if(param != 0)
+				this.result /= param;
+		}
+	}
+	
 	//получение результата
 	public int getResult() {
 		return this.result;
--- InteractRunner.java
+++ InteractRunner.java
@@ -5,17 +5,31 @@ public class InteractRunner {
 		Scanner reader = new Scanner(System.in);
 		try {
 			Calculator calc = new Calculator();
-			String exit = "no";
-			while (!exit.equals("yes")) {
-				System.out.println("Enter first argument: ");
-				String first = reader.next();
-				System.out.println("Enter second argument: ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+			String exit = "n";
+			while (!exit.equals("y")) {
+				System.out.println("What are u going to do?");
+				System.out.println("a - add, r - remove, m - multiply, d - divide");
+				String action = reader.next();
+				System.out.println("Enter argument: ");
+				String args = reader.next();
+				if(action.equals("a"))
+					calc.add(Integer.valueOf(args));
+				if(action.equals("r"))
+					calc.remove(Integer.valueOf(args));
+				if(action.equals("m"))
+					calc.mult(Integer.valueOf(args));
+				if(action.equals("d")) 
+					calc.div(Integer.valueOf(args));
 				System.out.println("Result: " + calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit: yes/no ");
+				System.out.println("Exit: y - yes/n - no ");
 				exit = reader.next();
+				if (exit.equals("n")) {
+					System.out.println("Clean result? ");
+					System.out.println("Exit: y - yes/n - no ");
+					String ans = reader.next();
+					if (ans.equals("y"))
+						calc.cleanResult();
+				}
 			}
 		}	finally {
 				reader.close();
